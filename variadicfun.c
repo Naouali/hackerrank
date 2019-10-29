808eaf6000c4c32e4c000a82c39a5b3cd79156c3
Naouali <elnaoualinebil@gmail.com>
2019-10-30 00:19:42 +0100

    variadic function in c, hacker rank problem's solution

---
+++ variadicfun.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MIN_ELEMENT 1
+#define MAX_ELEMENT 1000000
+int  sum (int count,...) {
+    va_list list;  //declaring a va_list type
+    int s = 0;
+    int i;
+
+    va_start(list,count); // initializing a va_list type which take the last knowing argument
+    for (i = 0; i < count; i++)
+        s = s + va_arg(list, int);  //va_arg - return thr next arfument
+    va_end(list);
+    return (s);
+
+}
+
+int min(int count,...) {
+
+return (1);
+}
+
+int max(int count,...) {
+return (1000000);
+}
